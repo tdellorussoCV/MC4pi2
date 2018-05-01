634fadf652143d3b8d5c3398c31b1623e9f6fad0
tdellorussoCV <38885677+tdellorussoCV@users.noreply.github.com>
2018-05-01 12:10:42 -0400

    adding Images

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 </head>
 
 <body>
-	
+
 	<table style="height:300px">
 		<tr>
 			<td>
-			<span><img src="/images/Commvault.jpg" alt="Commvault" style="banner" height="75" width="450"><h1>MC4 Team KPI's</h1><?php echo date('Y-m-d H:i:s'); ?></span>
+			<span><img src="/images/Commvault.jpg" alt="Commvault" style="banner" height="75" width="450"><h1>MC4 Team KPI's 	<?php echo date('Y-m-d H:i:s'); ?></h1></span>
 			</td>
 		</tr>
 	</table>
