b52b53a1d89f718136bdc4df16626b186777bd85
tdellorussoCV <38885677+tdellorussoCV@users.noreply.github.com>
2018-12-14 11:29:54 -0500

    Changed Date refresh
    Changed Date refresh

--- v2.2/src/js/app/carousel/carousel.component.js
+++ v2.2/src/js/app/carousel/carousel.component.js
@@ -2,33 +2,31 @@
 angular.module('carousel', ["ngSanitize"]).
 component('carousel', {
     templateUrl: 'templates/carousel.html',
-    controller: function($scope, $http, $interval,) {
+    controller: function($scope, $http, $interval, ) {
         $scope.imgSrc = [];
         $scope.imgType = null;
         //var for set initial index number in array
         $scope.ind = 0;
         var index = $scope.ind;
-        
+        var dynInt = '';
         //setting random to prevent disk caching 
         $scope.rndMath = new Date().getTime();
         $scope.imgUrl = 'http://172.19.21.99:81/slides/'
-
-        //setting random for no cache
-        $http.get("/json/carousel.json").then(function(res,) {
+        //Getting slide information
+        $http.get("http://172.19.21.99:81/json/carousel.json").then(function(res, ) {
             $scope.imgSrc = res.data;
             console.log(res.data);
-            console.log(index);
-            console.log(res.data[index].Extension);
             videochk();
-        });    
-        //setting for timer to switch slides
-        $interval(function(){            
-            $http.get("/json/carousel.json").then(function(res) {
-            $scope.imgSrc = res.data;
-            console.log(res.data);
-            $scope.rndMath = new Date().getTime();
         });
-        },300000);
+        //recurring Slide information
+        $interval(function() {
+            $http.get("http://172.19.21.99:81/json/carousel.json").then(function(res) {
+                $scope.imgSrc = res.data;
+                $scope.rndMath = new Date().getTime();
+            });
+        }, 300000);
+        //END
+        //setting for timer to switch slides
         $interval(function() {
             if ($scope.ind < ($scope.imgSrc.length - 1)) {
                 $scope.ind++;
@@ -37,21 +35,24 @@ component('carousel', {
                 $scope.ind = 0;
                 index = 0;
             }
-            console.log(index);
-            console.log($scope.imgSrc[index].Extension);
             videochk();
+            console.log()
             // console.log($scope.ind, $scope.imgSrc.length);
-        }, 15000);
+        }, 60000);
         //  video change over 
-        var videoSrc ='<video height="700px" width="100%" controls autoplay><source src="/slides/NEWSBREIF11-09-2018.mp4" type="video/mp4"></source></video>';
-		$scope.videoState = 'false';
-        function videochk(){
-            if ($scope.imgSrc[index].Extension === '.mp4'){
+        var videoSrc = '<video height="700px" width="100%" controls autoplay><source src="/slides/NEWSBREIF11-09-2018.mp4" type="video/mp4"></source></video>';
+        $scope.videoState = 'false';
+
+        function videochk() {
+            if ($scope.imgSrc[index].Extension === '.mp4') {
                 $scope.videoState = true
-                console.log($scope.videoState);
-            }else {$scope.videoState = false}
-        };        
+            } else {
+                $scope.videoState = false
+            }
+        };
         //End VideoChk 
+        //Interval Check
+        //End Interval Check
         // Image Resizer...
         var imageResizing = angular.module('imageResizingModule', [])
         // Image Resizing...
--- v2.2/src/js/app/header/header.component.js
+++ v2.2/src/js/app/header/header.component.js
@@ -1,16 +1,21 @@
 'use strict';
 angular.module('header').
 component('header', {
-    templateUrl: 'templates/header.html',
-    controller: function($scope, $interval) {
-        var days = moment().format("LL");
-        $scope.theDay = days;
-        $scope.theTime = new Date().toLocaleTimeString();
-        $interval(function() {
-            $scope.theTime = new Date().toLocaleTimeString();
-        }, 1000);
-        $interval(function() {
-            $scope.theDay = days;
-        }, 43200000)
-    }
-});
+            templateUrl: 'templates/header.html',
+            controller: function($scope, $interval) {
+                // var days = moment().format("LL");
+                $scope.theDay = moment().format("LL");
+                $scope.theTime = new Date().toLocaleTimeString();
+                console.log($scope.theDay, $scope.theTime);
+                $interval(function() {
+                    $scope.theTime = new Date().toLocaleTimeString();
+                    if ($scope.theTime === '6:15:00 AM') {
+                        $scope.theDay = moment().format("LL");
+                        console.log($scope.theDay, $scope.theTime);
+                    }
+                }, 1000);
+                //     $interval(function() {
+                //         $scope.theDay = moment().format("LL");
+                //     }, 43200000)
+                }
+            });
